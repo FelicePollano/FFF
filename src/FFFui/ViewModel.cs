1f0f64b2250bde00ec8ba2f81eaaa550ae2d7b06
Felice Pollano <felice@felicepollano.com>
2023-01-20 11:51:13 +0100

    progress on UI

--- src/FFFui/ViewModel.cs
+++ src/FFFui/ViewModel.cs
@@ -1,17 +1,61 @@
-﻿using System;
+﻿using Fff.Crawler;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace FFFui
 {
 
     internal class ViewModel : INotifyPropertyChanged
     {
+        class ChangePathCommand : ICommand
+        {
+            ViewModel model;
+            public ChangePathCommand(ViewModel model)
+            {
+                this.model = model;
+            }
+            public event EventHandler? CanExecuteChanged;
+
+            public bool CanExecute(object? parameter)
+            {
+                return true;
+            }
+
+            public void Execute(object? parameter)
+            {
+                
+
+                var dlg = new CommonOpenFileDialog();
+                dlg.Title = "Select Folder";
+                dlg.IsFolderPicker = true;
+                dlg.InitialDirectory = model.Path;
+
+                dlg.AddToMostRecentlyUsedList = false;
+                dlg.AllowNonFileSystemItems = false;
+                dlg.DefaultDirectory = model.Path;
+                dlg.EnsureFileExists = true;
+                dlg.EnsurePathExists = true;
+                dlg.EnsureReadOnly = false;
+                dlg.EnsureValidNames = true;
+                dlg.Multiselect = false;
+                dlg.ShowPlacesList = true;
+
+                if (dlg.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                   model.Path = dlg.FileName;
+                }
+            }
+        }
         class SearchCommand : ICommand
         {
             ViewModel model;
@@ -34,10 +78,39 @@ namespace FFFui
                 return !string.IsNullOrEmpty(model.ToSearch);
             }
 
-            public void Execute(object? parameter)
+            public async void Execute(object? parameter)
             {
                 model.Tabs.Add(new SearchViewModel(model.ToSearch));
                 model.Selected = model.Tabs.Count - 1;
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+
+                Regex regex;
+                Crawler crawler;
+                var files = string.IsNullOrEmpty(model.Types)?new string[0]: model.Types.Split(';');
+                if (model.UseRegex)
+                {
+                    try
+                    {
+                        regex = new Regex(model.ToSearch ,!model.MatchCase?RegexOptions.Compiled | RegexOptions.IgnoreCase : RegexOptions.Compiled);
+                        crawler = new Crawler(files.Length == 0 ? new[] { "*.*" } : files, regex, model.NameOnly);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.Error.WriteLine($"Cannot parse {model.ToSearch} as a regular expression:{e.Message}");
+                        return;
+                    }
+                }
+                else
+                {
+                    crawler = new Crawler(files.Length == 0 ? new[] { "*.*" } : files, model.ToSearch, !model.MatchCase, model.NameOnly);
+                }
+                crawler.Report = (results, file) => {
+                    
+                };
+                
+                await crawler.Crawl(model.Path);
+
             }
 
 
@@ -45,6 +118,8 @@ namespace FFFui
 
         private string toSearch;
         private int selected;
+        private string types;
+        private string path;
 
         public int Selected
         {
@@ -64,17 +139,39 @@ namespace FFFui
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ToSearch)));
             }
         }
-
+        public string Path
+        {
+            get { return path; }
+            set
+            {
+                path = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Path)));
+            }
+        }
+        public string Types
+        {
+            get { return types; }
+            set
+            {
+                types = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Types)));
+            }
+        }
 
         public ICommand Search { get; private set; }
+        public ICommand ChangePath { get; private set; }
 
         public ViewModel()
         {
+            Path = Directory.GetCurrentDirectory();
             Search = new SearchCommand(this);
+            ChangePath = new ChangePathCommand(this);
             tabs = new ObservableCollection<SearchViewModel>();
         }
         public event PropertyChangedEventHandler? PropertyChanged;
         private bool useRegex;
+        private bool matchCase;
+        private bool nameOnly;
         private ObservableCollection<SearchViewModel> tabs;
 
         public ObservableCollection<SearchViewModel>  Tabs
@@ -92,6 +189,30 @@ namespace FFFui
             set {
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(UseRegex)));
                 useRegex = value; 
+            }
+        }
+        public bool MatchCase
+        {
+            get
+            {
+                return matchCase;
+            }
+            set
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(MatchCase)));
+                matchCase = value;
+            }
+        }
+        public bool NameOnly
+        {
+            get
+            {
+                return nameOnly;
+            }
+            set
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(NameOnly)));
+                nameOnly = value;
             }
         }
 
