b66e557d9e2eb5957a3dcb02cfd14a2e1e2cff47
Felice Pollano <felice@felicepollano.com>
2023-04-17 17:26:07 +0200

    added behavior to select all eddit text as focused

---
+++ src/FFFui/Behaviors/SelectOnFocusBehavior.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace FFFui.Behaviors
+{
+    public class SelectOnFocusBehavior:Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.AssociatedObject.GotFocus += AssociatedObject_GotFocus;
+        }
+
+        private void AssociatedObject_GotFocus(object sender, System.Windows.RoutedEventArgs e)
+        {
+            (AssociatedObject as TextBox).SelectAll();
+        }
+
+        protected override void OnDetaching()
+        {
+            this.AssociatedObject.GotFocus -= AssociatedObject_GotFocus;
+            base.OnDetaching();
+        }
+    }
+}
